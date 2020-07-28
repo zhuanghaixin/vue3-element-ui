71bfd7c1da4b9183d9e2f8e18ac6fc2804f958c5
zhuanghaixin <649308293@qq.com>
2020-07-28 22:20:26 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hi</div>
+  <Frank></Frank>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
---
+++ src/components/Frank.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+       我的第一个组件
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "Frank"
+    }
+</script>
+
+<style scoped>
+
+</style>
