1c678718cbb1d954ba64c1e70c2af18241b38210
zhuanghaixin <649308293@qq.com>
2020-07-29 18:21:50 +0800

    feat(Topnav.vue/Doc.vue/Home.vue): 初始化aside的html，调节aside和topnav的位置

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
+import Switch from  './components/Switch.vue'
+import Dialog from './components/Dialog.vue'
+import Tabs from  './components/Tabs.vue'
+import Button from './components/Button.vue'
 import './index.scss'
 import {createWebHashHistory,createRouter} from 'vue-router'
 const history=createWebHashHistory()
@@ -9,7 +13,15 @@ const router=createRouter({
     history:history,
     routes: [
         {path:'/',component:Home},
-        {path:'/doc',component:Doc},
+        {path:'/doc',component:Doc,
+            children:[
+                { path:'switch',component:Switch},
+                { path:'dialog',component:Dialog},
+                { path:'button',component:Button},
+                { path:'tabs',component:Tabs},
+            ]
+        },
+
     ],
 })
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>轱辘UI</h1>
       <h2>一个很厉害的UI框架</h2>
       <p class="actions">
-        <a href>Github</a>
-        <a href>开始</a>
+        <a href="https://github.com/zhuanghaixin/vue3-element-ui">Github</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -24,4 +24,25 @@ export default {
 </script>
 
 <style scoped lang="scss">
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions{
+      padding: 8px 0;
+      a {
+        margin:0 8px;
+        background: #fff;
+        display: inline-block;
+        $h: 28px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 8px;
+      }
+    }
+  }
 </style>
